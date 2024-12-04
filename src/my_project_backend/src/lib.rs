d067460d182c4fe2cab4bbac3b58d058551f381b
Franciszek Skrobot <franek.skrobot@gmail.com>
2024-12-04 09:40:09 +0000

    init

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,3 +1,21 @@
+use::std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk::update]
+fn setMsg(newMsg: String) {
+    MSG.with(|message| {
+        *message.borrow_mut() = newMsg;
+    })
+}
+
+#[ic_cdk::query]
+fn getMsg() -> String {
+    MSG.with(|msg| msg.borrow().clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
